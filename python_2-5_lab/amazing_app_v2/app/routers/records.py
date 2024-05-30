91ab16bd27073ef47be46bacd70b856893b02502
Arthur <153835086+arthvrm@users.noreply.github.com>
2024-05-30 10:51:11 +0300

    Delete python_2-5_lab/amazing_app_v2 directory

--- python_2-5_lab/amazing_app_v2/app/routers/records.py
+++
@@ -1,8 +0,0 @@
-from app import schemes, crud
-from app.database import get_db
-
-from fastapi import APIRouter, Depends, HTTPException
-
-from sqlalchemy.orm import Session
-
-records_router = APIRouter(prefix="/records", tags=["records"])
